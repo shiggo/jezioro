5dcaf47b91942a8795c9990ee89d2e1913522c71
Adam Jeziorski <ajezzz@gmail.com>
2020-09-04 14:57:25 -0400

    final edits for v0.20

--- R/quickGAM.R
+++ R/quickGAM.R
@@ -3,13 +3,14 @@
 #'@description
 #'\code{quickGAM} is a function to fit a GAM and perform the related analyses described in Simpson (2018). This function essentially applies the code provided in that article's Supplementary Information, to a pair of vectors.
 #'
-#'@usage quickGAM(x, y, xLabel = NULL, yLabel = NULL, yRange = NULL)
+#'@usage quickGAM(x, y, xLabel = NULL, yLabel = NULL, yRange = NULL, skipAuto = FALSE)
 #'
 #'@param x vector containing the independent data/linear predictor
 #'@param y vector containing the dependent data/response variable
 #'@param xLabel (defaults to NULL): optional custom string to ensure consistent x-axis labels across the plots
 #'@param yLabel (defaults to NULL): optional custom string to ensure consistent y-axis labels across the plots
 #'@param yRange (defaults to NULL): optional custom range to ensure consistent y-axis across all plots
+#'@param skipAuto (Defaults to FALSE): logical to indicate whether to skip the autocorrelation calculation and plot
 #'
 #'@details Two input vectors are required (x and y, the independent and dependent variable respectively). The optional arguments allow customization of the x and y axes labels to ensure they remain consistent across all graphs. Similarly the y-range can be manually defined.
 #'
@@ -43,7 +44,7 @@
 #'
 #'@export
 
-quickGAM <- function(x, y, xLabel=NULL, yLabel=NULL, yRange=NULL){
+quickGAM <- function(x, y, xLabel=NULL, yLabel=NULL, yRange=NULL, skipAuto = FALSE){
   #Set default ggplot theme
   theme_set(theme_bw())
   
@@ -84,28 +85,33 @@ quickGAM <- function(x, y, xLabel=NULL, yLabel=NULL, yRange=NULL){
   #Model assumes the residuals are independent, so to account for residual temporal autcorrelation, can include a continuous-time first-order autoregressive (CAR(1)) process in the model residuals
   mod <- gamm(depen ~ s(indep, k=15), data=plotData, correlation = corCAR1(form = ~ indep), method="REML")
   
-  #Estimate of phi and confidence interval
-  #Estimated value of phi for the CAR(1) can be extracted from the fit model via the $lme component.
-  smallPhi <- intervals(mod$lme, which = "var-cov")$corStruct
-  smallPhi
+
   
   #Summary object
   #The returned object includes both the linear mixed model and GAM elements of the model, need to access these elements individually.
   #Output shows estimated complexity of the fitted smooth, in terms of the effective degreees of freedom of the spline. An associated  F statistic and test of the null hypothesis are also shown.
   summary(mod$gam)
   
-  #Plot CAR(1) process
-  #In example 1 (chlorophyll a data), exponential decline in correlation with increasing separation is evident, little dependence once samples are ~5 cm apart
-  S <-seq(0,50, length=100)
-  car1 <- setNames(as.data.frame(t(outer(smallPhi, S, FUN = `^`)[1, , ])), c("Lower","Correlation","Upper"))
-  car1 <- transform(car1, S = S)
+  #Plot CAR(1) process (can be bypassed using the skipAuto argument)
+  if (skipAuto == F){
+    
+    #Estimate of phi and confidence interval
+    #Estimated value of phi for the CAR(1) can be extracted from the fit model via the $lme component.
+    smallPhi <- intervals(mod$lme, which = "var-cov")$corStruct
+    smallPhi
+    
+    #In example 1 (chlorophyll a data), exponential decline in correlation with increasing separation is evident, little dependence once samples are ~5 cm apart
+    S <-seq(0,50, length=100)
+    car1 <- setNames(as.data.frame(t(outer(smallPhi, S, FUN = `^`)[1, , ])), c("Lower","Correlation","Upper"))
+    car1 <- transform(car1, S = S)
   
-  car1Plt <- ggplot(car1, aes(x = S, y = Correlation)) +
-    geom_ribbon(aes(ymax = Upper, ymin = Lower), fill = "black", alpha = 0.2) +
-    geom_line() +
-    ylab(expression(italic(c) * (list(h, varphi)))) +
-    xlab(bquote(h ~"("*.(indepVarLabel)*")"))
-  print(car1Plt)
+    car1Plt <- ggplot(car1, aes(x = S, y = Correlation)) +
+      geom_ribbon(aes(ymax = Upper, ymin = Lower), fill = "black", alpha = 0.2) +
+      geom_line() +
+      ylab(expression(italic(c) * (list(h, varphi)))) +
+      xlab(bquote(h ~"("*.(indepVarLabel)*")"))
+    print(car1Plt)
+  }
   
   #Plot the fitted GAM
   #General idea is to predict from the fitted model for a fine grid of points over the range of the linear predictor (i.e. time or depth). The plot is of the trend with an ~95% point-wise confidence interval that assumes asymptotic normality.
